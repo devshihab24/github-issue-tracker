ea279ba22549035ad9009440a587c52da9eabbb2
shihabbabu420 <mdshihabbabu478@gmail.com>
2026-03-08 19:59:14 +0600

    first commit

---
+++ login/login.js
@@ -0,0 +1,18 @@
+document.getElementById("login-btn").addEventListener("click", (e) => {
+  e.preventDefault();
+  let userName = document.getElementById("login-username");
+  let password = document.getElementById("login-password");
+//   console.log(userName, password);
+  if (
+    userName.value.toLowerCase() == "admin" &&
+    password.value.toLowerCase() == "admin123"
+  ) {
+    window.location.assign("../home/home.html")
+    userName.value = "";
+    password.value = "";
+  } else {
+    alert("Invalid Inputs");
+    userName.value = "";
+    password.value = "";
+  }
+});
