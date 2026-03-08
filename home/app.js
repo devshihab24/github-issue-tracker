41f4b7db6aecf79f31f3b3c3e8649af8d32dba39
shihabbabu420 <mdshihabbabu478@gmail.com>
2026-03-09 01:23:59 +0600

    modal section is completed

--- home/app.js
+++ home/app.js
@@ -1,7 +1,11 @@
 // variable declaration section
 const allCategoryBtns = document.querySelectorAll(".category-btn");
 const cardsContainer = document.getElementById("cards-container");
+const loader = document.getElementById("loader")
 let issueCount = document.getElementById("issue-count");
+const issueDetailsModal = document.getElementById('issueDetailsModal')
+const modalDetail = document.getElementById('detail')
+let badgeKind = null;
 
 // active button functionality
 allCategoryBtns.forEach((btn) => {
@@ -10,7 +14,6 @@ allCategoryBtns.forEach((btn) => {
     removeActiveClass();
     btn.classList.add("btn-primary");
   });
-  loadIssues();
 });
 
 function removeActiveClass() {
@@ -31,31 +34,73 @@ async function loadIssues() {
 
 // load category wise data
 async function loadCategoryIssues(status) {
+
   const res = await fetch(
     "https://phi-lab-server.vercel.app/api/v1/lab/issues",
   );
   const data = await res.json();
-  let categoryData = data.data
-  if(status !== "all"){
-    categoryData = data.data.filter(item=>item.status === status)
+  let categoryData = data.data;
+  if (status !== "all") {
+    categoryData = data.data.filter((item) => item.status === status);
   }
   displayIssuesData(categoryData);
+}
+// load data based on id and show data on modal
+async function loadDetails(id) {
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+    const data = await res.json()
+    // console.log(data.data);
+    badgeKind = badgeType(data.data)
+    modalDetail.innerHTML = `
+              <h2 class="text-xl md:text-2xl heading-color font-bold">
+                ${data.data.title}
+              </h2>
+              <div class="flex justify-start items-center gap-5">
+                <div class="badge badge-success uppercase text-white">
+                  ${data.data.status}
+                </div>
+                <p class="bullet capitalize">Opened by ${data.data.author.split('_').join(" ")}</p>
+                <p class="bullet">${data.data.createdAt.split("T")[0]}</p>
+              </div>
+              <div class="flex gap-5">
+                ${labelBadge(data.data.labels)}
+              </div>
+              <p class="p-color text-lg">
+                ${data.data.description}
+              </p>
+              <div class="flex items-center bg-[#F8FAFC] rounded-md">
+                <div class="w-1/2 p-5">
+                  <p class="p-color">Assignee:</p>
+                  <h5 class="text-lg font-semibold heading-color capitalize">
+                    ${data.data.author.split('_').join(" ")}
+                  </h5>
+                </div>
+                <div class="w-1/2 p-5">
+                  <p class="p-color">Priority:</p>
+                  <div class="badge ${badgeKind} uppercase text-white">
+                    ${data.data.priority}
+                  </div>
+                </div>
+              </div>
+    
+    `
+
+    issueDetailsModal.showModal()
 }
 
 // display loaded data on ui
 const displayIssuesData = (issues) => {
-    cardsContainer.innerHTML = ''
+  cardsContainer.innerHTML = "";
   issueCount.innerText = `${issues.length} Issues`;
   issues.forEach((issue) => {
     // console.log(issue);
     const div = document.createElement("div");
-    div.className = `issue-card shadow-md ${issue.status == "open" ? "border-[#00A96E] border-t-4 border-t-[#00A96E]" : "border-[#A855F7] border-t-4 border-t-[#A855F7]"} p-4 rounded-md space-y-3`;
-    let badgeKind = null;
-    if (issue.priority.toLowerCase() == "low") badgeKind = "badge-primary";
-    else if (issue.priority.toLowerCase() == "medium")
-      badgeKind = "badge-warning";
-    else badgeKind = "badge-error";
-
+    // display modal functionality
+    div.addEventListener('click', ()=>{
+        loadDetails(issue.id)
+    })
+    div.className = `issue-card hover:shadow-2xl bg-white cursor-pointer shadow-md ${issue.status == "open" ? "border-[#00A96E] border-t-4 border-t-[#00A96E]" : "border-[#A855F7] border-t-4 border-t-[#A855F7]"} p-4 rounded-md space-y-3`;
+    badgeKind = badgeType(issue)
     div.innerHTML = `
             <div class="flex justify-between items-center">
               <img src="../assets/Open-Status.png" alt="" />
@@ -82,6 +127,13 @@ const displayIssuesData = (issues) => {
     cardsContainer.append(div);
   });
 };
+function badgeType(issue){
+    
+    if (issue.priority.toLowerCase() == "low") return "badge-primary";
+    else if (issue.priority.toLowerCase() == "medium")
+      return "badge-warning";
+    else return "badge-error";
+}
 
 function labelBadge(labels) {
   const badge = labels.map(
@@ -90,5 +142,7 @@ function labelBadge(labels) {
   );
   return badge.join(" ");
 }
+
+// loader functionality
 
 loadIssues();
